f30a7334c2355ce40390130b873fa30ff16f8c7e
[Jacob McGinniss] <J_MCGINNISS@uncg.edu>
2026-06-30 21:27:53 -0400

    merge with main

---
+++ Swap-A-Bookaroo/src/main/java/com/backend_API/Swap-A-Bookaroo/SummerPrototypeApplication.java
@@ -0,0 +1,13 @@
+package com.csc340.summer_prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummerPrototypeApplication.class, args);
+	}
+
+}
