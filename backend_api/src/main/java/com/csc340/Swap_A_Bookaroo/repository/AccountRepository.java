493ddc30b30bcd2cc3cbb38019944c56ae1b6cf6
[Jacob McGinniss] <J_MCGINNISS@uncg.edu>
2026-07-10 20:47:11 -0400

    Add logic to AccountRepository

---
+++ backend_api/src/main/java/com/csc340/Swap_A_Bookaroo/repository/AccountRepository.java
@@ -0,0 +1,13 @@
+package com.csc340.Swap_A_Bookaroo.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.csc340.Swap_A_Bookaroo.entities.Account;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findByUsername(String username);
+}
