c3c5faf19384f632f2a0e913611d15cc07337191
Yera <yeraldinet1234@gmail.com>
2026-07-15 23:54:12 -0400

    Condense SwapRequestStatus enum declaration
    Refactor SwapRequestStatus enum to a single line format.

--- backend_api/src/main/java/com/csc340/Swap_A_Bookaroo/entities/SwapRequestStatus.java
+++ backend_api/src/main/java/com/csc340/Swap_A_Bookaroo/entities/SwapRequestStatus.java
@@ -1,13 +1,2 @@
 package com.csc340.Swap_A_Bookaroo.entities;
-<<<<<<< HEAD
-
-public enum SwapRequestStatus {
-    PENDING,
-    APPROVED,
-    REJECTED,
-    CANCELLED,
-    COMPLETED
-}
-=======
 public enum SwapRequestStatus { PENDING, APPROVED, REJECTED, CANCELLED, COMPLETED }
->>>>>>> origin/main
