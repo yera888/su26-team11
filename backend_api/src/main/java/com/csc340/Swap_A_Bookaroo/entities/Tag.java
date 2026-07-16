052ea18ade92eef894ee3e12562b166bc7085092
Yera <yeraldinet1234@gmail.com>
2026-07-15 23:54:27 -0400

    Refactor Tag class for cleaner imports and methods

--- backend_api/src/main/java/com/csc340/Swap_A_Bookaroo/entities/Tag.java
+++ backend_api/src/main/java/com/csc340/Swap_A_Bookaroo/entities/Tag.java
@@ -1,15 +1,6 @@
 package com.csc340.Swap_A_Bookaroo.entities;
 
-<<<<<<< HEAD
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
-=======
 import jakarta.persistence.*;
->>>>>>> origin/main
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,29 +12,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Tag {
 
-<<<<<<< HEAD
-    // Data fields
-=======
->>>>>>> origin/main
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long tagId;
 
     private String tagName;
 
-<<<<<<< HEAD
-    // Methods
-    public void createTag(String tagName) {
-        this.tagName = tagName;
-    }
-
-    public void updateTag(String tagName) {
-        this.tagName = tagName;
-    }
-
-}
-=======
     public void createTag(String tagName) { this.tagName = tagName; }
     public void updateTag(String tagName) { this.tagName = tagName; }
 }
->>>>>>> origin/main
