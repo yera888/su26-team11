0d41631f44b9840b1dfa308f89c2fbd70fce7fcb
Yera <yeraldinet1234@gmail.com>
2026-07-15 23:56:03 -0400

    Refactor SwapRequestRepository to add new methods

--- backend_api/src/main/java/com/csc340/Swap_A_Bookaroo/repository/SwapRequestRepository.java
+++ backend_api/src/main/java/com/csc340/Swap_A_Bookaroo/repository/SwapRequestRepository.java
@@ -1,22 +1,4 @@
 package com.csc340.Swap_A_Bookaroo.repository;
-<<<<<<< HEAD
-
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.csc340.Swap_A_Bookaroo.entities.SwapRequest;
-import com.csc340.Swap_A_Bookaroo.entities.SwapRequestStatus;
-
-@Repository
-public interface SwapRequestRepository extends JpaRepository<SwapRequest, Long> {
-
-    List<SwapRequest> findByBookListing_ProviderProfile_ProviderProfileIdAndStatus(Long providerProfileId,
-            SwapRequestStatus status);
-
-}
-=======
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -28,4 +10,3 @@ public interface SwapRequestRepository extends JpaRepository<SwapRequest, Long> 
     List<SwapRequest> findByBookListing_ProviderProfile_ProviderProfileIdAndStatusIn(Long id, List<SwapRequestStatus> statuses);
     List<SwapRequest> findByCustomerProfile_CustomerProfileIdAndStatus(Long customerProfileId, SwapRequestStatus status);
 }
->>>>>>> origin/main
