a2e6a3c2232dbc8814a5d4a66638fed4e2b9d1f6
Yera <yeraldinet1234@gmail.com>
2026-07-15 23:55:43 -0400

    Refactor ProviderProfileRepository to remove duplicates
    Removed duplicate import statements and cleaned up the code.

--- backend_api/src/main/java/com/csc340/Swap_A_Bookaroo/repository/ProviderProfileRepository.java
+++ backend_api/src/main/java/com/csc340/Swap_A_Bookaroo/repository/ProviderProfileRepository.java
@@ -1,26 +1,10 @@
 package com.csc340.Swap_A_Bookaroo.repository;
-<<<<<<< HEAD
-
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-=======
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
->>>>>>> origin/main
 import com.csc340.Swap_A_Bookaroo.entities.ProviderProfile;
 
 @Repository
 public interface ProviderProfileRepository extends JpaRepository<ProviderProfile, Long> {
-<<<<<<< HEAD
-
-    Optional<ProviderProfile> findByAccount_AccountId(Long accountId);
-
-}
-=======
     Optional<ProviderProfile> findByAccount_AccountId(Long accountId);
 }
->>>>>>> origin/main
