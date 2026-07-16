2c9b39cf3d90bcf3c228f6db365d5610faf42599
[Jacob McGinniss] <J_MCGINNISS@uncg.edu>
2026-07-15 22:34:53 -0400

    Add sign in and sign up

--- backend_api/src/main/java/com/csc340/Swap_A_Bookaroo/service/AccountService.java
+++ backend_api/src/main/java/com/csc340/Swap_A_Bookaroo/service/AccountService.java
@@ -1,16 +1,28 @@
 package com.csc340.Swap_A_Bookaroo.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.csc340.Swap_A_Bookaroo.entities.Account;
+import com.csc340.Swap_A_Bookaroo.entities.CustomerProfile;
+import com.csc340.Swap_A_Bookaroo.entities.ProviderProfile;
 import com.csc340.Swap_A_Bookaroo.repository.AccountRepository;
+import com.csc340.Swap_A_Bookaroo.repository.CustomerProfileRepository;
+import com.csc340.Swap_A_Bookaroo.repository.ProviderProfileRepository;
 
 @Service
 public class AccountService {
 
     private final AccountRepository accountRepository;
+    private final CustomerProfileRepository customerProfileRepository;
+    private final ProviderProfileRepository providerProfileRepository;
 
-    public AccountService(AccountRepository accountRepository) {
+    public AccountService(AccountRepository accountRepository, 
+                          CustomerProfileRepository customerProfileRepository,
+                          ProviderProfileRepository providerProfileRepository) {
         this.accountRepository = accountRepository;
+        this.customerProfileRepository = customerProfileRepository;
+        this.providerProfileRepository = providerProfileRepository;
     }
 
     public Account getAccountById(Long accountId) {
@@ -39,5 +51,26 @@ public class AccountService {
             return true;
         }
         return false;
+    }
+
+    @Transactional
+    public Account registerDualAccount(Account account){
+        // Check if username is in use
+        if (accountRepository.findByUsername(account.getUsername()).isPresent()) {
+            return null;
+        }
+        // Assign role
+        account.setRole("USER");
+        Account savedAccount = accountRepository.save(account);
+        // Create Customer Profile
+        CustomerProfile customerProfile = new CustomerProfile();
+        customerProfile.setAccount(savedAccount);
+        customerProfileRepository.save(customerProfile);
+        // Create Provider Profile
+        ProviderProfile providerProfile = new ProviderProfile();
+        providerProfile.setAccount(savedAccount);
+        providerProfileRepository.save(providerProfile);
+
+        return savedAccount;
     }
 }
---
+++ backend_api/src/main/java/com/csc340/Swap_A_Bookaroo/uiController/AccountUiController.java
@@ -0,0 +1,103 @@
+package com.csc340.Swap_A_Bookaroo.uiController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.csc340.Swap_A_Bookaroo.entities.Account;
+import com.csc340.Swap_A_Bookaroo.entities.CustomerProfile;
+import com.csc340.Swap_A_Bookaroo.entities.ProviderProfile;
+import com.csc340.Swap_A_Bookaroo.service.AccountService;
+import com.csc340.Swap_A_Bookaroo.service.BookListingService;
+import com.csc340.Swap_A_Bookaroo.service.CustomerProfileService;
+import com.csc340.Swap_A_Bookaroo.service.ProviderProfileService;
+import com.csc340.Swap_A_Bookaroo.service.SwapRequestService;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/account")
+public class AccountUiController {
+    // Datafield
+    private final AccountService accountService;
+    private final BookListingService bookListingService;
+    private final CustomerProfileService customerProfileService;
+    private final ProviderProfileService providerProfileService;
+    private final SwapRequestService swapRequestService;
+
+    // Constructors
+    @Autowired
+    public AccountUiController(AccountService accountService,
+                                BookListingService bookListingService,
+                                CustomerProfileService customerProfileService,
+                                ProviderProfileService providerProfileService,
+                                SwapRequestService swapRequestService) {
+        this.accountService = accountService;
+        this.bookListingService = bookListingService;
+        this.customerProfileService = customerProfileService;
+        this.providerProfileService = providerProfileService;
+        this.swapRequestService = swapRequestService;
+    }
+
+    // Methods
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute Account loginRequest, Model model, HttpSession session) {
+        Account account = accountService.login(loginRequest.getUsername(), loginRequest.getPassword());
+        if(account != null){
+            // If username and password is right, 
+            session.setAttribute("user", account);
+            return "redirect:/profile";
+        }else {
+            // If username or password is wrong, send a error message and keep them on login
+            model.addAttribute("errorMessage","Invalid username or password.");
+            return "login";
+        }
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/account/login";
+    }
+
+    @GetMapping("/signup")
+    public String register(Model model) {
+        model.addAttribute("account", new Account());
+        return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String registerCustomer(@ModelAttribute Account account, Model model, HttpSession session) {
+        Account createdAccount = accountService.registerDualAccount(account);
+        if(createdAccount != null){
+            session.setAttribute("customerId", createdAccount);
+
+            CustomerProfile customerProfile = customerProfileService.getCustomerByAccountId(createdAccount.getAccountId());
+            ProviderProfile providerProfile = providerProfileService.getProviderProfileByAccountId(createdAccount.getAccountId());
+            
+            session.setAttribute("customerProfileId", customerProfile.getCustomerProfileId());
+            session.setAttribute("providerProfileId", providerProfile.getProviderProfileId());
+
+            return "redirect:/customer/profile";
+        } else{
+            // Username already exists
+            model.addAttribute("errorMessage", "Username is already taken. Please try another one.");
+            model.addAttribute("account", account); 
+            return "signup";
+        }
+    }
+
+
+}
