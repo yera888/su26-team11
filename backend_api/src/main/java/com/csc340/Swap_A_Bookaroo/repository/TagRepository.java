858c9593d9d084fc3b68cd0ca86dddece03adeb3
Yera <yeraldinet1234@gmail.com>
2026-07-15 23:56:17 -0400

    Refactor TagRepository to remove duplicate imports
    Removed duplicate import statements and cleaned up the code.

--- backend_api/src/main/java/com/csc340/Swap_A_Bookaroo/repository/TagRepository.java
+++ backend_api/src/main/java/com/csc340/Swap_A_Bookaroo/repository/TagRepository.java
@@ -1,26 +1,10 @@
 package com.csc340.Swap_A_Bookaroo.repository;
-<<<<<<< HEAD
-
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-=======
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
->>>>>>> origin/main
 import com.csc340.Swap_A_Bookaroo.entities.Tag;
 
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
-<<<<<<< HEAD
-
-    Optional<Tag> findByTagName(String tagName);
-
-}
-=======
     Optional<Tag> findByTagName(String tagName);
 }
->>>>>>> origin/main
