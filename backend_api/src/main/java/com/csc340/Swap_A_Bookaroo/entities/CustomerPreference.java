a381fabb897a73e1e29aaf0bc17d3bcfbb1958d5
Yera <yeraldinet1234@gmail.com>
2026-07-15 23:52:08 -0400

    Refactor CustomerPreference class with Lombok
    Refactor CustomerPreference class to use Lombok annotations and streamline imports.

--- backend_api/src/main/java/com/csc340/Swap_A_Bookaroo/entities/CustomerPreference.java
+++ backend_api/src/main/java/com/csc340/Swap_A_Bookaroo/entities/CustomerPreference.java
@@ -1,22 +1,6 @@
 package com.csc340.Swap_A_Bookaroo.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-<<<<<<< HEAD
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-
-@Entity
-public class CustomerPreference extends Tag {
-
-    @ManyToOne
-    @JsonIgnoreProperties({ "preferences" })
-    @JoinColumn
-    private CustomerProfile customerProfile;
-
-}
-=======
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -31,4 +15,3 @@ public class CustomerPreference extends Tag {
     @JsonIgnoreProperties({ "preferences", "account" })
     private CustomerProfile customerProfile;
 }
->>>>>>> origin/main
