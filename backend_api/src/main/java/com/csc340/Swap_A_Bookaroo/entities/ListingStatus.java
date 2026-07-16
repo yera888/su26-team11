8d121c1c9faf5a1db7b5a24975ca0dbd5ab456d3
Yera <yeraldinet1234@gmail.com>
2026-07-15 23:53:05 -0400

    Simplify ListingStatus enum declaration
    Refactored ListingStatus enum to a single line format.

--- backend_api/src/main/java/com/csc340/Swap_A_Bookaroo/entities/ListingStatus.java
+++ backend_api/src/main/java/com/csc340/Swap_A_Bookaroo/entities/ListingStatus.java
@@ -1,12 +1,2 @@
 package com.csc340.Swap_A_Bookaroo.entities;
-<<<<<<< HEAD
-
-public enum ListingStatus {
-    AVAILABLE,
-    REQUESTED,
-    SWAPPED,
-    REMOVED
-}
-=======
 public enum ListingStatus { AVAILABLE, REQUESTED, SWAPPED, REMOVED }
->>>>>>> origin/main
