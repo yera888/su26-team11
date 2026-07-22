769d711e2364a3940020ef6b6c70efad7e2ba3b7
yera888 <yeraldinet1234@gmail.com>
2026-07-22 09:14:02 -0400

    Added request queries to swap request repository

--- backend_api/src/main/java/com/csc340/Swap_A_Bookaroo/repository/CustomerProfileRepository.java
+++ backend_api/src/main/java/com/csc340/Swap_A_Bookaroo/repository/CustomerProfileRepository.java
@@ -1,4 +1,5 @@
 package com.csc340.Swap_A_Bookaroo.repository;
+
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,4 +8,5 @@ import com.csc340.Swap_A_Bookaroo.entities.CustomerProfile;
 @Repository
 public interface CustomerProfileRepository extends JpaRepository<CustomerProfile, Long> {
     Optional<CustomerProfile> findByAccount_AccountId(Long accountId);
+    Optional<CustomerProfile> findByAccount_Username(String username);
 }
