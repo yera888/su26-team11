b9e29375af52758e78e237914c6e875bfeb072d0
[Jacob McGinniss] <J_MCGINNISS@uncg.edu>
2026-07-22 22:46:53 -0400

    Up to date

---
+++ backend_api/src/main/java/com/csc340/Swap_A_Bookaroo/entities/CustomerPreference.java
@@ -0,0 +1,17 @@
+package com.csc340.Swap_A_Bookaroo.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class CustomerPreference extends Tag {
+
+    @ManyToOne
+    @JoinColumn(name = "customer_profile_id")
+    @JsonIgnoreProperties({ "preferences", "account" })
+    private CustomerProfile customerProfile;
+}
